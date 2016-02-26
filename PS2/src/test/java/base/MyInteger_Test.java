15b481c45cfa84b611ba7f9bc84916f664c7fbbc
NotandorSS <jkmorris@udel.edu>
2016-02-26 11:45:45 -0500

    Update MyInteger_Test.java

--- PS2/src/test/java/base/MyInteger_Test.java
+++ PS2/src/test/java/base/MyInteger_Test.java
@@ -28,7 +28,27 @@ public class MyInteger_Test {
 
 	@Test
 	public void test() {
-		assertTrue(1==1);
+		int n11 = 11;
+		int n16 = 16;
+		MyInteger m1 = new MyInteger (1);
+		MyInteger m11 = new MyInteger (11);
+		MyInteger c11 = new MyInteger (11);
+		
+		assertTrue("Test1 isEven(int) failed",MyInteger.isEven(n11)==false);
+		assertFalse("Test1 isEven(int) failed",MyInteger.isEven(n11));
+		
+		assertTrue("Test1 isOdd(int) failed",MyInteger.isOdd(n11));
+		assertTrue("Test2 isOdd(int) failed",MyInteger.isOdd(n16) == false);
+		
+		assertTrue("Test1 isPrime(int) failed",MyInteger.isPrime(n11));
+		assertTrue("Test2 isPrime(int) failed",MyInteger.isPrime(n16) == false);
+		
+		assertTrue("Test1 MyI.equals(int) failed", m1.equals(n11) == false);
+		assertTrue("Test1 MyI.equals(int) failed", m11.equals(n11));
+		
+		assertTrue("Test1 MyI.equals(MyI) failed", m1.equals(m11) == false);
+		assertTrue("Test1 MyI.equals(MyI) failed", m11.equals(c11));
+		
 	}
 
 }
